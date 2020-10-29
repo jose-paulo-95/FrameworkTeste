c3edc444ba6198c7e4f5ff4f2d2398fdbbc67f6f
Jose Paulo Luiz Junior <josepauloluizjr@gmail.com>
2020-10-28 21:22:25 -0300

    Inclusao do metodos2

---
+++ pageobjtecs-testes/src/test/java/pages/Metodos2.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Metodos2 {
+	
+	WebDriver driver;
+	
+	public void navegador (String url, String navegador) {
+		
+		
+	}
+
+}
